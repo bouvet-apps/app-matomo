6e97d4bea83e949cd27558516a3091c916427779
Anders Nodeland Indregard <57173860+andersnin@users.noreply.github.com>
2023-11-24 14:04:17 +0100

    feature/BOUENO-9: PR-edits

--- src/main/resources/admin/widgets/matomo/matomo.js
+++ src/main/resources/admin/widgets/matomo/matomo.js
@@ -11,17 +11,41 @@ const settings = {
 
 const token = app.config['matomo.token'];
 
+function getUniqueVisitors(date, period, matomoConfig){
+  const response = libs.httpClient.request({
+    url: `https:${matomoConfig.config.matomoUrl}/?module=API&method=VisitsSummary.getUniqueVisitors&date=${date}&period=${period}&idSite=${matomoConfig.config.siteId}&format=${settings.responseFormat}&token_auth=${token}`
+  })
+  return JSON.parse(response.body).value;
+}
+
+function getCountryGraph(date, period, matomoConfig){
+  return `https:${matomoConfig.config.matomoUrl}/?module=API&method=ImageGraph.get&idSite=${matomoConfig.config.siteId}&apiModule=UserCountry&apiAction=getCountry&graphType=horizontalBar&period=${period}&date=${date}&width=500&height=250&format=${settings.responseFormat}&token_auth=${token}`;
+}
+
+function getPageUrl(date, period, req, matomoConfig) {
+  if (req.params.contentId && matomoConfig.config.domainName) {
+    const contentPath = libs.content.get({ key: req.params.contentId })._path;
+    const sitePath = libs.content.getSite({ key: req.params.contentId })._path;
+    const relativeContentPath = contentPath.replace(sitePath, '');
+    const domainName = matomoConfig.config.domainName;
+    const pageUrl = 'https://' + domainName + relativeContentPath
+
+    const response = libs.httpClient.request({
+      url: `https:${matomoConfig.config.matomoUrl}/?module=API&method=Actions.getPageUrl&pageUrl=${pageUrl}&date=${date}&period=${period}&idSite=${matomoConfig.config.siteId}&format=${settings.responseFormat}&token_auth=${token}`
+    })
+
+    let data = JSON.parse(response.body);
+    return data[0]; // Always returns array with one object
+  } else {
+    throw new Error("Failed fetching report")
+  }
+}
+
 exports.get = function (req) {
   if (!req.params.contentId) {
-    const errorView = resolve("./error-handler/error.html");
-    const errorModel = {
-      styleUri: libs.portal.assetUrl({
-        path: "matomo.css"
-      }),
-    };
     return {
-      body: libs.thymeleaf.render(errorView, errorModel),
-      contentType: 'text/html'
+      contentType: "text/html",
+      body: "<widget class='error'>No content selected</widget>"
     };
   }
 
@@ -36,41 +60,11 @@ exports.get = function (req) {
       path: "matomo.css"
     }),
     pageData: {
-      yesterday: getPageUrl('yesterday', 'day'),
-      lastWeek: getPageUrl('lastWeek', 'day'),
-      lastMonth: getPageUrl('lastMonth', 'day')
+      yesterday: getPageUrl('yesterday', 'day', req, matomoConfig),
+      lastWeek: getPageUrl('lastWeek', 'day', req, matomoConfig),
+      lastMonth: getPageUrl('lastMonth', 'day', req, matomoConfig)
     },
   };
-
-  function getPageUrl(date, period) {
-    if (req.params.contentId && matomoConfig.config.domainName) {
-      const contentPath = libs.content.get({ key: req.params.contentId })._path;
-      const sitePath = libs.content.getSite({ key: req.params.contentId })._path;
-      const relativeContentPath = contentPath.replace(sitePath, '');
-      const domainName = matomoConfig.config.domainName;
-      const pageUrl = 'https://' + domainName + relativeContentPath
-
-      const response = libs.httpClient.request({
-        url: `https:${matomoConfig.config.matomoUrl}/?module=API&method=Actions.getPageUrl&pageUrl=${pageUrl}&date=${date}&period=${period}&idSite=${matomoConfig.config.siteId}&format=${settings.responseFormat}&token_auth=${token}`
-      })
-
-      let data = JSON.parse(response.body);
-      return data[0]; // Always returns array with one object
-    } else {
-      throw new Error("Failed fetching report")
-    }
-  }
-
-  function getUniqueVisitors(date, period){
-    const response = libs.httpClient.request({
-      url: `https:${matomoConfig.config.matomoUrl}/?module=API&method=VisitsSummary.getUniqueVisitors&date=${date}&period=${period}&idSite=${matomoConfig.config.siteId}&format=${settings.responseFormat}&token_auth=${token}`
-    })
-    return JSON.parse(response.body).value;
-  }
-
-  function getCountryGraph(date, period){
-    return `https:${matomoConfig.config.matomoUrl}/?module=API&method=ImageGraph.get&idSite=${matomoConfig.config.siteId}&apiModule=UserCountry&apiAction=getCountry&graphType=horizontalBar&period=${period}&date=${date}&width=500&height=250&format=${settings.responseFormat}&token_auth=${token}`;
-  }
 
   return {
     body: libs.thymeleaf.render(view, model),
